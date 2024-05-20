7b0d484d1d2a942c19cc0d70e7e958a347f2bf7f
Jockei <159291450+Jockei@users.noreply.github.com>
2024-05-20 08:42:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let x = Math.random() * 15;
+document.getElementById("coluna_1").innerHTML = x;
+
+let y = Math.floor ((Math.random() * 30) + 15);
+document.getElementById("coluna_2").innerHTML = y;
+
+let z = Math.floor ((Math.random() * 45) + 30);
+document.getElementById("coluna_3").innerHTML = z;
+
+let a = Math.floor ((Math.random() * 60) + 45);
+document.getElementById("coluna_4").innerHTML = a;
+
+let b = Math.floor ((Math.random() * 75) + 60);
+document.getElementById("coluna_5").innerHTML = b;
